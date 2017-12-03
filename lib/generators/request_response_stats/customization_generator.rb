c1d87979b2c9c3d711bb78d345937e46cf6fab5a
Munish Goyal <munishapc@gmail.com>
2017-12-03 17:39:29 +0800

    Correct customization generator description

--- lib/generators/request_response_stats/customization_generator.rb
+++ lib/generators/request_response_stats/customization_generator.rb
@@ -2,8 +2,7 @@ module RequestResponseStats
   module Generators
     class CustomizationGenerator < Rails::Generators::Base
       source_root File.expand_path('../../templates', __FILE__)
-
-      desc "Create RequestResponseStats configuration files"
+      desc "Create RequestResponseStats customization files"
 
       def copy_customization_files
         files_to_generate = [
@@ -12,6 +11,7 @@ module RequestResponseStats
           "lib/tasks/move_req_res_cycle_data_from_redis_to_mongo.rake"
         ]
         files_to_generate.each do |file|
+          puts "Generating #{file}.."
           just_file_name = file.split("/")[-1]
           copy_file just_file_name, file
         end
